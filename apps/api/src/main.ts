60954c84660f79f2e25159a972044df670efc6fb
matevzh <matevz.prasnikar04@gmail.com>
2026-03-11 12:16:48 +0100

    Add logging to requests and responses

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: false,
+  }));
+
+  // Log every request and response
+  app.use((req, res, next) => {
+    logger.log(`${req.ip} ${req.method} ${req.url}`);
+    res.on('finish', () => {
+      logger.log(`${req.ip} ${req.method} ${req.url} ${res.statusCode}`);
+    });
+    next();
+  });
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
